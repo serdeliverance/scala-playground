3c09dfc24f3959d4b9a4248caa1c23644cd8af89
Sergio Cano <canosergio90@gmail.com>
2023-04-26 15:11:34 +0200

    IO exercises implementation
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- ce3Playground/src/main/scala/part2effects/IOIntro.scala
+++ ce3Playground/src/main/scala/part2effects/IOIntro.scala
@@ -5,9 +5,18 @@ import cats.effect.IO
 object IOIntro:
     def sequenceTakeLast[A, B](ioa: IO[B], iob: IO[B]): IO[B] =
         ioa.flatMap(_ => iob)
+    
+    def sequenceTakeLast_v2[A, B](ioa: IO[B], iob: IO[B]): IO[B] =
+        ioa *> iob // "andThen"
+
+    def sequenceTakeLast_v3[A, B](ioa: IO[B], iob: IO[B]): IO[B] =
+        ioa >> iob // "andThen"
 
     def sequenceTakeFirst[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
         ioa.flatMap(a => iob.map(_ => a))
+
+    def sequenceTakeFirs_v2[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
+        ioa <* iob
 
     def sequenceTakeFirst_v2[A, B](ioa: IO[A], iob: IO[B]): IO[A] =
         for
@@ -17,9 +26,28 @@ object IOIntro:
 
     def forever[A](ioa: IO[A]): IO[A] =
         ioa.flatMap(_ => forever(ioa))
+    
+    def forever_v2[A](ioa: IO[A]): IO[A] =
+        ioa >> forever_v2(ioa)
+    
+    def forever_v3[A](ioa: IO[A]): IO[A] =
+        ioa *> forever_v3(ioa)
+
+    def forever_v4[A](ioa: IO[A]): IO[A] =
+        ioa.foreverM
 
     def convert[A, B](ioa: IO[A], value: B): IO[B] =
         ioa.map(_ => value)
 
+    def convert_v2[A, B](ioa: IO[A], value: B): IO[B] =
+        ioa.as(value)
+
     def asUnit[A](ioa: IO[A]): IO[Unit] =
-        ioa.map(_ => ())
+        ioa.as(())
+
+    @main def run: Unit =
+        import cats.effect.unsafe.implicits.global
+        forever_v3(IO {
+            println("forever!")
+            Thread.sleep(100)
+        }).unsafeRunSync()
